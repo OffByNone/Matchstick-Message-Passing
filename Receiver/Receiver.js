2cdc877acd0a1bb61bec0a65b4f39bd5e1a1f93f
OffByNone <jeremy.mcneil@gmail.com>
2015-06-16 19:27:45 -0500

    testing ground work for screen sharing over webrtc
    this is quite old, and doesn't work.  but at one point I believe I was
    close.

--- Receiver/Receiver.js
+++ Receiver/Receiver.js
@@ -1,24 +1,53 @@
 "use strict";
 (function() {
-    //if the below two lines are not executed the matchstick will think it 
+    //if the below two lines are not executed the matchstick will think it
     //failed to open the app and return to the default screen after a timeout
-    
+
     var receiverManager = new ReceiverManager("~MessagePassingDemo"); //create a new ReceiverManager with the same app id used in the sender
-    var messageChannel = receiverManager.createMessageChannel("messagePassingDemo");    
+    var messageChannel = receiverManager.createMessageChannel("messagePassingDemo");
     var messagesContainer = document.getElementById("messagesContainer");
-    
+    var createdAnswe;
+    var info = document.getElementById("info");
+
     messageChannel.on("message", function(senderId, data){
         var message = JSON.parse(data);
         var messageContainer =  document.createElement("div");
         messageContainer.className = "message";
         messageContainer.innerHTML = "Message Received!<br />data: " + message.data + "<br />senderId:" + senderId;
         messagesContainer.appendChild(messageContainer);
-        
-        //messages can be of any type. Including complex josn objects;
-        var message = { data: 'receiver app says hello' };
-
-        messageChannel.send(JSON.stringify(message)); //messages must be stringified if json
+        createdAnswer();
+        setTimeout(function(){
+            info.innerHTML += "<br />" + JSON.stringify(createdAnswe) + "here!";
+            messageChannel.send(JSON.stringify(createdAnswe)); //messages must be stringified if json
+    },1500);
     });
-    
-    receiverManager.open();    
+
+    function createdAnswer(){
+        var PeerConnection = window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+        var configuration = {};
+        var options = {};
+
+        var pc = new PeerConnection(configuration,options);
+
+        var channelName = "screenSharingTest";
+        var channelOptions = {};
+
+        pc.createDataChannel(channelName,channelOptions);
+
+        var constraints = {
+            offerToReceiveAudio:true,
+            offerToReceiveVideo:true
+        };
+        pc.setRemoteDescription(new SessionDescription(JSON.parse('put the offer here')));
+        pc.createAnswer(function (answer) {
+            pc.setLocalDescription(answer);
+            createdAnswe = answer;
+        },
+        function(error){
+            info.innerHTML += "<br />error:" + JSON.stringify(error);
+        },
+        constraints);
+    }
+
+    receiverManager.open();
 })();
--- Sender/Sender.js
+++ Sender/Sender.js
@@ -2,44 +2,45 @@
 (function() {
     var appid = "~MessagePassingDemo"; //Unique id of your application, must start with a ~
     var matchstickIPAddress = "192.168.1.13"; //IP address of the matchstick
-    var receiverAppUrl = "http://192.168.1.2:8080/Matchstick-Message-Passing/Receiver/Receiver.html"; //Url of the page to load on the receiver
+    var receiverAppUrl = "http://192.168.1.4/Matchstick-Message-Passing/Receiver/Receiver.html"; //Url of the page to load on the receiver
     var timeout = -1; //after not communicating with the sender for this many milliseconds return to the default matchstick screen. -1 means don't timeout
     var useInterprocessCommunication = true; //not sure what this means for my application
     var isRunning = false;
     var messageChannel; //used to send messages between sender and receiver
     var senderDaemon = new SenderDaemon(matchstickIPAddress, appid); //comes from the sender api, is the object which will be used to communicate with the matchstick
     var messagesContainer = document.getElementById("messagesContainer");
-    
-    
+
+
     senderDaemon.on("appopened", function (channel) {
         messageChannel = channel;
 
         messageChannel.on("message",function(message){
-            //We don't need to JSON.parse the parameter this function takes in 
-            //as the api is already doing that for us.  But it only does that for the sender, 
+            //We don't need to JSON.parse the parameter this function takes in
+            //as the api is already doing that for us.  But it only does that for the sender,
             //not for the receiver so you will still need to run JSON.parse on the receiver side
-            
+
             var messageContainer =  document.createElement("div");
             messageContainer.className = "message";
-            messageContainer.innerHTML = "Message Received!<br />data: " + message.data;
+            messageContainer.innerHTML = "Message Received!<br />data: " + message;
+            console.log(JSON.stringify(message));
             messagesContainer.appendChild(messageContainer);
         });
     });
 
-    document.getElementById("toggleAppStatus").onclick  = function(){
+    document.getElementById("toggleAppStatus").onclick = function(){
         if(isRunning)
         {
             document.getElementById("postMessage").className = "disabled";
             this.innerHTML = "Launch App";
             senderDaemon.closeApp();
         }
-        else 
+        else
         {
             document.getElementById("postMessage").className = "";
             this.innerHTML = "Close App";
             senderDaemon.openApp(receiverAppUrl, timeout, useInterprocessCommunication);
         }
-        
+
         isRunning = !isRunning;
     };
     document.getElementById("postMessage").onclick = function(){
@@ -48,7 +49,7 @@
             alert("App must be running on receiver before you can send a message.\nPlease click launch app, then try again");
             return;
         }
-        
+
         //messages can be of any type. Including complex josn objects;
         var message = {
             data: 'sender app says hello'
